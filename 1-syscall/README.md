eab1fac789fcddc470399a7568d59579462f86b7
jackeryli <jack.yenchun.li@gmail.com>
2024-02-26 18:01:50 -0800

    basic shell

---
+++ 1-syscall/README.md
@@ -0,0 +1,4 @@
+# Syscall & Shell
+
+- stderr no buffer
+- stdout has buffer, and it will accumulate until `\n`
--- 1-syscall/shell.c
+++ 1-syscall/shell.c
@@ -6,10 +6,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
+#include <signal.h> /* signal handler*/
 #include <errno.h>
 
-int main(int argc, char **argv)
+/**
+ * Read stdin into buf
+*/
+char*
+gets(char* buf, int max)
 {
+	int i, cc;
+	char c;
+
+	for(i=0; i+1 < max;){
+		cc = read(0, &c, 1);
+		if(cc < 1)
+			break;
+		buf[i++] = c;
+		if(c == '\n' || c == '\r')
+			break;
+	}
+	buf[i] = '\0';
+	return buf;
+}
+
+int
+getcmd(char* buf, int nbuf)
+{
+	fprintf(stderr, "$ ");
+	memset(buf, 0, nbuf);
+	gets(buf, nbuf);
+	if(buf[0] == 0)
+		return -1;
+	return 0;
+}
+
+void
+signal_handler(int sig)
+{
+	if(sig == SIGINT){
+		exit(1);
+	}
+}
+
+int
+main(void)
+{
+	static char buf[100];
+
+	signal(SIGINT, signal_handler);
+
+	while(getcmd(buf, sizeof(buf)) >= 0){
+		printf("%s", buf);
+	}
+
 	return 0;
 }
